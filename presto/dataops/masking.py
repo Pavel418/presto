12c3ca677d01302e6874400155304056c404a5b1
Pavel418 <pavel@vvv.az>
2025-04-07 13:17:39 +0400

    Update masking.py

--- presto/dataops/masking.py
+++ presto/dataops/masking.py
@@ -41,7 +41,10 @@ def make_mask(strategy: str, mask_ratio: float) -> Tuple[np.ndarray]:
 
     # SRTM is included here, but ignored by Presto
     mask = np.full((NUM_TIMESTEPS, len(BANDS_GROUPS_IDX)), False)
-    num_tokens_to_mask = int((NUM_TIMESTEPS * len(BANDS_GROUPS_IDX)) * mask_ratio)
+    print(f"mask shape: {mask.shape}")
+    num_tokens_to_mask = int(((NUM_TIMESTEPS * len(BANDS_GROUPS_IDX)) + 1) * mask_ratio)
+    print(f"num_tokens_to_mask not multiplied by mask_ratio: {int((NUM_TIMESTEPS * len(BANDS_GROUPS_IDX)) + 1)}")
+    print(f"num_tokens_to_mask: {num_tokens_to_mask}")
 
     def mask_topography(num_tokens_to_mask, mask_ratio):
         should_flip = random() < mask_ratio
