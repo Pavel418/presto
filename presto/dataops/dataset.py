130cc7881b1fa0d501eab3ea84dcf90582beb5e6
Pavel418 <pavel@vvv.az>
2025-04-08 13:21:51 +0400

    Update dataset.py

--- presto/dataops/dataset.py
+++ presto/dataops/dataset.py
@@ -640,6 +640,7 @@ class FranceCropsFullDataset(TorchDataset):
             expanded_dataset = expanded_dataset.shuffle(seed=self.seed)
         # Convert to Presto format
         processed_dataset = expanded_dataset.map(self._convert_to_presto)
+        print(f"[DATASET] dataset entry shape: {processed_dataset[0]['x'].shape}")
         return processed_dataset
 
     def __len__(self) -> int:
